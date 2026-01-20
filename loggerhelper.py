1ac37396f7dcbb0999488986ffc41aeb17ab9157
aaronelroyy <elroy1310@gmail.com>
2026-01-20 23:06:42 +0530

    2

--- loggerhelper.py
+++ loggerhelper.py
@@ -3,4 +3,5 @@ import logging
 logger = logging.getLogger(
     __name__
 )  # here it is logger is loggerhelper name = main or imported file
+logger.propagate = False  # default true
 logger.info("hello from helper")
--- loggings.py
+++ loggings.py
@@ -1,11 +1,12 @@
 import logging
 
+"""
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%m/%d/%Y %H:%M:%S",
 )
-"""
+
 logging.debug("This is a debug message")
 logging.info("This is a info message")
 logging.warning("This is a warning message")  # defualt levl is warning n above
@@ -13,4 +14,35 @@ logging.error("This is a error message")
 logging.critical("This is a critical message")
 """
 
-import loggerhelper
+logger = logging.getLogger(__name__)
+
+stream_h = logging.StreamHandler()
+file_h = logging.FileHandler("file.log")
+
+stream_h.setLevel(logging.WARNING)
+file_h.setLevel(logging.ERROR)
+
+formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+stream_h.setFormatter(formatter)
+file_h.setFormatter(formatter)
+
+logger.addHandler(stream_h)
+logger.addHandler(file_h)
+
+logger.warning("this is a warning")
+logger.error("this is an error")
+
+# https://chatgpt.com/c/696fb60a-11f4-8320-8919-ff3ff9b1a68b explanation
+
+"""
+                    👦 LOGGER
+               (warning / error)
+                         |
+            --------------------------------
+            |                              |
+      📺 StreamHandler               📒 FileHandler
+   (WARNING and up)                 (ERROR and up)
+            |                              |
+       shows on screen              writes to file.log
+
+"""
